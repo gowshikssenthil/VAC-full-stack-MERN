1a2290cd5ab7c39b5cb6474c4ad7aeadef633120
gowshik <gowshikssenthil@gmail.com>
2026-06-04 15:52:32 +0530

    js variables ,css , data types and opreators

---
+++ css/javascript/script.js
@@ -0,0 +1,100 @@
+var a=10;
+let b=20;
+const c=30;
+
+console.log(a);
+console.log(b);
+console.log(c); 
+
+var num = 10;
+var str ="gowshik";
+var undefined;
+var nullValue = null;
+var booleanValue = true;
+
+CONSOLE.log(typeof num);
+CONSOLE.log(typeof str);
+CONSOLE.log(typeof undefined);
+CONSOLE.log(typeof nullValue);
+CONSOLE.log(typeof booleanValue);
+
+var bigint =123n;
+vr symbol=symbol("gowshik");
+
+console.log(typeof bigint);
+console.log(typeof symbol);
+
+var arr=[10,20,30];
+console.log(typrof arr, arr);
+console.log(typrof arr, arr);
+var obj ={
+    name:"gowshik",
+    age: 25,
+    city: "chennai"
+};
+console.log(typeof obj, obj);
+
+ //Arithmetic operators(=,-+,*,/,%,**)
+
+ var a=10;
+ var b=20;
+ console.log(a+b);
+ console.log(a-b);
+ console.log(a*b);
+ console.log(a/b);
+ console.log(a%b);   
+ console.log(a**b);
+
+ //logical operators(&&,||,!)
+
+ var x=true;
+ var y=false;
+
+ console.log(x&&y);
+ console.log(x||y);
+ console.log(!x);
+  
+
+ var a=true
+    var b=false;
+
+    console.log(a&&b);
+    console.log(a||b);
+    console.log(!a);
+
+    //Relational operators(>,<,>=,<=,==,===,!=,!==)
+
+    var a=10;
+    var b=20;               
+
+    console.log(a>b);
+    console.log(a<b);
+    console.log(a>=b);              
+    console.log(a<=b);
+    console.log(a==b);
+    console.log(a===b);
+    console.log(a!=b);
+    console.log(a!==b);
+
+    //assignment operators(=,+=,-=,*=,/=,%=,**=)
+
+    a=10;
+    b=20;
+
+    a+=b;  
+    console.log(a);
+
+    a-=b;
+    console.log(a);             
+
+    a*=b;
+    console.log(a); 
+
+    a/=b;
+    console.log(a);
+
+    a%=b;
+    console.log(a);
+
+    a**=b;  
+    console.log(a);
